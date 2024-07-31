c0ec24f42a708f1e6c7d8a6e96434d81a625247d
anhpls <anh.h.duy@gmail.com>
2024-07-31 04:26:36 -0700

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,96 @@
 import './App.css';
 import cart from './shopping.png';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
 
   const [inputValue, setInputValue] = useState("");
   const [groceryItems, setGroceryItems] = useState([]);
+  const [isCompleted, setIsCompleted] = useState(false);  
 
-  /* 
-    name: "Banana",
-    quantity: 1,
-    completed: true
+  useEffect(() => {determineCompletedStatus();}, [groceryItems]);
 
-  */
+  function determineCompletedStatus (){
+    if (!groceryItems.length){
+      return setIsCompleted(false);
+    }
 
+    let isAllCompleted = true;
+
+    groceryItems.forEach(item => {
+      if(!item.completed) isAllCompleted = false;
+    });
+
+    setIsCompleted(isAllCompleted);
+  }
 
 
   function handleInputValueChange(e){
     setInputValue(e.target.value);
   }
 
+  function handleAddGroceryItem(e){
+    if(e.key === 'Enter'){
+      if (inputValue) {
+        const updatedGroceryList = [...groceryItems];
+
+        const itemIndex = updatedGroceryList.findIndex(item => item.name === inputValue)
+
+        if(itemIndex === -1) {
+          updatedGroceryList.push({
+            name: inputValue,
+            quantity: 1,
+            completed: false,   
+          })
+        } else {
+          updatedGroceryList[itemIndex].quantity++
+        }
+        setGroceryItems(updatedGroceryList)
+        setInputValue("");
+        }
+
+      }
+  }
+  
+  
+  function handleDeleteGrocery (name){
+    setGroceryItems([...groceryItems].filter(item => item.name !== name));
+  }
+
+  function handleUpdateStatus (status, index){
+    const updatedGroceryList = [...groceryItems];
+    updatedGroceryList[index].completed = status
+    setGroceryItems(updatedGroceryList)
+  }
+
+  function renderGroceryList (){
+    return groceryItems.map((item, index) => (
+      <li key={item.name}>
+      <div className='container'>
+        <input type='checkbox' onChange={(e) => {handleUpdateStatus(e.target.checked, index);}} value={item.completed} checked={item.completed}/>
+        <p>{item.name} {item.quantity > 1 ? <span>x{item.quantity}</span> : null}</p>
+      </div>
+      <div>
+        <button className='remove-button' onClick={() => handleDeleteGrocery(item.name)}>X</button>
+      </div>
+    </li>
+    ))
+  }
+
+
   return (
     <main className="App">
         <div>
         <div>
-          <h4 className='success'>You're Done</h4>
+          {isCompleted && <h4 className='success'>You're Done</h4>}
           <div className='header'>
             <h1>Shopping List</h1>
             <img src={cart} alt='shopping-cart' className='shopping-cart'/>
-            <input type="text" placeholder='Add an Item...' className='item-input' onChange={handleInputValueChange} value={inputValue}/>
+            <input type="text" placeholder='Add an Item...' className='item-input' onChange={handleInputValueChange} value={inputValue} onKeyDown={handleAddGroceryItem}/>
           </div>
         </div>
         <ul>
-          <li>
-            <div className='container'>
-              <input type='checkbox'/>
-              <p>Carrots</p>
-            </div>
-            <div>
-              <button className='remove-button'>X</button>
-            </div>
-          </li>
+          {renderGroceryList()}
         </ul>
         </div>
     </main>
